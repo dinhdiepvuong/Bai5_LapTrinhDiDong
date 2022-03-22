ae85bbd79239028f9259b881f6da8d82fb11937a
dinhdiepvuong <diepvuong.py@gmail.com>
2022-03-22 07:03:02 +0700

    cau1_finsh

---
+++ app/src/main/java/edu/iuh/a18086981_dinhdiepvuong/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.iuh.a18086981_dinhdiepvuong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
